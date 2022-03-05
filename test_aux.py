d1676979226fc7e3946577084b4d4a856e57e9e2
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-06 00:34:38 +0800

    try: try bool mask

--- test_aux.py
+++ test_aux.py
@@ -33,16 +33,17 @@ class TestAux(unittest.TestCase):
         print(len(inputs))
         print()
         print(sequence_output)
-        # train_dataset = MyDataset(data=x["train_aux_trigger_items"], tokenizer=tokenizer, max_len=256)
-        # train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
-        # for item in train_loader:
-        #     input_ids, input_mask, input_seg, start_index, end_index = \
-        #             item["input_ids"], item["input_mask"], item["input_seg"], item["start_index"], item["end_index"]
-        #     print(input_ids)
-        #     print(input_mask)
-        #     print(input_seg)
-        #     encoder_rep = roberta(input_ids=input_ids, attention_mask=input_mask, token_type_ids=input_seg)[0]  # (bsz, seq, dim)
-        #     print(encoder_rep)
-        #     break
+
+        train_dataset = MyDataset(data=x["train_aux_trigger_items"], tokenizer=tokenizer, max_len=256)
+        train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
+        for item in train_loader:
+            input_ids, input_mask, input_seg, start_index, end_index = \
+                    item["input_ids"], item["input_mask"], item["input_seg"], item["start_index"], item["end_index"]
+            print(input_ids)
+            print(input_mask)
+            print(input_seg)
+            encoder_rep = roberta(input_ids=input_ids, token_type_ids=input_seg)[0]  # (bsz, seq, dim)
+            print(encoder_rep)
+            break
 if __name__ == "__main__":
     unittest.main()
