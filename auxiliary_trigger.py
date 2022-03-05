56dfe031b375905e5f25f12397705067ca61bf4d
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-05 21:54:58 +0800

    feature: learning rate decay strategy

--- auxiliary_trigger.py
+++ auxiliary_trigger.py
@@ -1,6 +1,7 @@
 from cmath import tanh
 from paddle.io import DataLoader,Dataset
 from paddlenlp.transformers import RobertaTokenizer,RobertaModel
+from paddle import optimizer
 import paddle
 import paddle.nn
 import sys
@@ -87,6 +88,26 @@ class MyModel(paddle.nn.Layer):
             avg_loss = paddle.mean(sum_loss)
             return avg_loss
 
+# learning rate decay strategy
+class WarmUp_LinearDecay:
+    def __init__(self, optimizer: optimizer.AdamW, init_rate, warm_up_steps, decay_steps, min_lr_rate):
+        self.optimizer = optimizer
+        self.init_rate = init_rate
+        self.warm_up_steps = warm_up_steps
+        self.decay_steps = decay_steps
+        self.min_lr_rate = min_lr_rate
+        self.optimizer_step = 0
+
+    def step(self):
+        self.optimizer_step += 1
+        if self.optimizer_step <= self.warm_up_steps:
+            rate = (self.optimizer_step / self.warm_up_steps) * self.init_rate
+        elif self.warm_up_steps < self.optimizer_step <= (self.warm_up_steps + self.decay_steps):
+            rate = (1.0 - ((self.optimizer_step - self.warm_up_steps) / self.decay_steps)) * self.init_rate
+        else:
+            rate = self.min_lr_rate
+        self.optimizer.set_lr(rate)
+        self.optimizer.step()
 
 
 if __name__ == "__main__":
