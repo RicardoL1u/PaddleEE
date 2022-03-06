4656d480427415896793be24af18388b6421f8b7
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-06 17:08:06 +0800

    feature: lr decay strategy for dominant

--- dominant_trigger.py
+++ dominant_trigger.py
@@ -136,3 +136,24 @@ class MyModel(paddle.nn.Layer):
             span_loss = (-paddle.log(span_prob + self.epsilon)) * span_label
             avg_span_loss = self.beta * paddle.sum(span_loss) / (paddle.nonzero(span_label, as_tuple=False).shape[0])
             return avg_se_loss + avg_span_loss
+
+# learning rate decay strategy
+class WarmUp_LinearDecay:
+    def __init__(self, optimizer: optimizer.AdamW, init_rate, warm_up_steps, decay_steps, min_lr_rate):
+        self.optimizer = optimizer
+        self.init_rate = init_rate
+        self.warm_up_steps = warm_up_steps
+        self.decay_steps = decay_steps
+        self.min_lr_rate = min_lr_rate
+        self.optimizer_step = 0
+
+    def step(self):
+        self.optimizer_step += 1
+        if self.optimizer_step <= self.warm_up_steps:
+            rate = (self.optimizer_step / self.warm_up_steps) * self.init_rate
+        elif self.warm_up_steps < self.optimizer_step <= (self.warm_up_steps + self.decay_steps):
+            rate = (1.0 - ((self.optimizer_step - self.warm_up_steps) / self.decay_steps)) * self.init_rate
+        else:
+            rate = self.min_lr_rate
+        self.optimizer.set_lr(rate)
+        self.optimizer.step()
