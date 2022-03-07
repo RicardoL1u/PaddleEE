4cb2ed76a74bddc8a11eafcd4203a191b73902d7
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-07 14:02:06 +0800

    feature: lr strategy for arguments

--- argument.py
+++ argument.py
@@ -116,3 +116,25 @@ class MyModel(paddle.nn.Layer):
             sum_loss = cls_loss + span_loss
             avg_loss = paddle.mean(sum_loss)
             return avg_loss
+
+    
+# learning rate decay strategy
+class WarmUp_LinearDecay:
+    def __init__(self, optimizer: optimizer.AdamW, init_rate, warm_up_steps, decay_steps, min_lr_rate):
+        self.optimizer = optimizer
+        self.init_rate = init_rate
+        self.warm_up_steps = warm_up_steps
+        self.decay_steps = decay_steps
+        self.min_lr_rate = min_lr_rate
+        self.optimizer_step = 0
+
+    def step(self):
+        self.optimizer_step += 1
+        if self.optimizer_step <= self.warm_up_steps:
+            rate = (self.optimizer_step / self.warm_up_steps) * self.init_rate
+        elif self.warm_up_steps < self.optimizer_step <= (self.warm_up_steps + self.decay_steps):
+            rate = (1.0 - ((self.optimizer_step - self.warm_up_steps) / self.decay_steps)) * self.init_rate
+        else:
+            rate = self.min_lr_rate
+        self.optimizer.set_lr(rate)
+        self.optimizer.step()
