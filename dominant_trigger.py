fix: bug for span softmax

dominant_trigger.py
@@ -120,7 +120,7 @@ class MyModel(paddle.nn.Layer):
         end_prob_seq = paddle.nn.functional.softmax(end_logits, axis=1)
         
         span_logits = util.masked_fill(span_logits,span_mask==0,-1e30)
-        span_prob = paddle.nn.functional.softmax(span_logits,axis=-1) # (bsz,seq,seq)
+        span_prob = paddle.nn.functional.softmax(span_logits,axis=1) # (bsz,seq,seq)
         
         # if there is no answers, returen the predict results
         if start_seq_label is None or end_seq_label is None or span_label is None or seq_mask is None:

joint_predict.py
@@ -51,7 +51,7 @@ class DomTrigger(paddle.nn.Layer):
         end_prob_seq = paddle.nn.functional.softmax(end_logits, axis=-1)  # (bsz, seq, 2)
         # 使用span_mask
         span_logits = util.masked_fill(span_logits, span_mask == 0, -1e30)
-        span_prob = paddle.nn.functional.softmax(span_logits, axis=-1)  # (bsz, seq, seq)
+        span_prob = paddle.nn.functional.softmax(span_logits, axis=1)  # (bsz, seq, seq)
         return start_prob_seq, end_prob_seq, span_prob
 
 

test_joint.py
@@ -3,6 +3,10 @@ import pickle
 import joint_predict
 from paddlenlp.transformers import BertTokenizer,BertModel
 import paddle
+import paddle.nn
+import torch.nn.functional
+
+from util import paddle2torch
 
 class TestJoint(unittest.TestCase):
     def __init__(self, methodName: str = ...) -> None:
@@ -25,9 +29,21 @@ class TestJoint(unittest.TestCase):
             print(len(trigger_input['span_mask']))
             # print(trigger_input['span_mask'][0][:][11:70])
             print(trigger_input['span_mask'][0][11][11:70])
-            print(trigger_input['span_mask'][0][10][11:70])
+            print(trigger_input['span_mask'][0][10][11:70]) ## all zero
             break
-
+    
+    def test_span(self):
+        seq_len = 4
+        span1_logits = paddle.randn([1,seq_len,1])
+        span2_logits = paddle.randn([1,seq_len])
+        span_logits = paddle.tile(span1_logits,repeat_times=[1, 1, seq_len]) + paddle.tile(span2_logits[:, None, :],repeat_times=[1, seq_len, 1])
+        print(span1_logits)
+        print(paddle.tile(span1_logits,repeat_times=[1, 1, seq_len]))
+        print(span2_logits)
+        print(paddle.tile(span2_logits[:, None, :],repeat_times=[1, seq_len, 1]))
+        print(span_logits)
+        print(paddle.nn.functional.softmax(span_logits, axis=1))  # (bsz, seq, seq)
+        print(torch.nn.functional.softmax(paddle2torch(span_logits), dim=1))  # (bsz, seq, seq)
 
 if __name__ == "__main__":
     unittest.main()

util.py
@@ -1,9 +1,11 @@
 # paddlepaddle >= 2.0
 import paddle
+import torch
 
 
 def masked_fill(x, mask, value):
     y = paddle.full(x.shape, value, x.dtype)
     return paddle.where(mask, y, x)
 
-
+def paddle2torch(x:paddle.Tensor)->torch.Tensor:
+    return torch.from_numpy(x.cpu().numpy())